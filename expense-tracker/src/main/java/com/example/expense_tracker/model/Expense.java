d0601d2b93d9e1c7b8c66942f3f5e9f2effa5733
MutyaSowmya123 <sowmya.mutya@slu.edu>
2026-01-01 22:28:33 -0600

    Finalize dashboard UI and chart integration

--- expense-tracker/src/main/java/com/example/expense_tracker/model/Expense.java
+++ expense-tracker/src/main/java/com/example/expense_tracker/model/Expense.java
@@ -1,11 +1,13 @@
 package com.example.expense_tracker.model;
 
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import java.time.LocalDate;
 
 @Entity
 public class Expense {
@@ -15,8 +17,12 @@ public class Expense {
     private Long id;
 
     private String description;
+
     private double amount;
+
     private String category;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate date;
 
     // Getters and Setters
